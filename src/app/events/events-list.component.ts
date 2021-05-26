8bcc130c44137b6243a9e6ca2d6f3b2879c8d6f8
satyx <satyamcrj@gmail.com>
2021-05-26 20:59:02 +0530

    Child Component

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr/>
+        <event-thumbnail [event]="event1" ></event-thumbnail>
+    </div>
+    `
+})
+export class EventsListComponent{
+    event1 = {
+        id:1,
+        name:'Satyam Sharma',
+        date:'26/05/2021',
+        time:'20:33',
+        price: '399.99',
+        imageUrl:'/assets/images/angularconnect-shield.png',
+        location: {
+            address: 'abc',
+            city: 'def',
+            country: 'Ghi'
+        }
+    }
+}
