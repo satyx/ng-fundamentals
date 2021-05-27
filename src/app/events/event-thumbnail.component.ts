93073be22798c8aa88e6acbc13a6e6310bdb8825
satyx <satyamcrj@gmail.com>
2021-05-27 23:16:15 +0530

    ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,13 +6,13 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     <div class="well hoverwall thumbnail">
         <h2>{{event.name}}</h2>
         <div>Date:{{event.date}}</div>
-        <div>Time:{{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div [ngSwitch]="event?.time">
-            <span *ngSwitchCase="'8:00 am'">Early Start</span>
-            <span *ngSwitchCase="'10:00 am'">Mid Start</span>
-            <span *ngSwitchDefault>Late Start</span>
+        <div [ngClass]="getStartTime()" [ngSwitch]="event?.time">
+            Time:{{event.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Mid Start)</span>
+            <span *ngSwitchDefault>(Late Start)</span>
         </div>
+        <div>Price: \${{event.price}}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event.location.address}}</span>
             <span class="pad-left">{{event.location.city}},{{event.location.country}}</span>
@@ -23,6 +23,8 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     </div>
     `,
     styles:[`
+        .bold {font-weight: bold;}
+        .green {color: #003300 !important;}
         .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb;}
@@ -31,4 +33,17 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 export class EventThumbnailComponent{
     @Input() event:any;
+    getStartTime(){
+        //also works
+        // const isEarlyStart = this.event && this.event.time==='8:00 am';
+        // return {green:isEarlyStart, bold:isEarlyStart};
+        // if(this.event && this.event.time==='8:00 am')
+        //     return 'green bold';
+        // return '';
+        
+        if(this.event && this.event.time==='8:00 am')
+            return ['green', 'bold'];
+        return [];
+
+    }
 }
