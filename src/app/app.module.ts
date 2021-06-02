f3e5fd1db924ae753ce3e88314d824f8aaff7196
satyx <satyamcrj@gmail.com>
2021-06-02 17:56:22 +0530

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
 import { RouterModule } from '@angular/router';
 import { appRoutes } from 'src/routes';
 import { CreateEventComponent } from './events/shared/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 
 @NgModule({
   imports: [
@@ -23,9 +25,10 @@ import { CreateEventComponent } from './events/shared/create-event.component';
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
-  providers:[EventService, ToastrService],
+  providers:[EventService, ToastrService,EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from "@angular/router";
+import { Error404Component } from "./app/errors/404.component";
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
+import { EventRouteActivator } from "./app/events/event-details/event-route-activator.service";
 import { EventsListComponent } from "./app/events/events-list.component";
 import { CreateEventComponent } from "./app/events/shared/create-event.component";
 
 export const appRoutes:Routes = [
     {path:'events',component:EventsListComponent},
     {path:'events/new',component:CreateEventComponent},
-    {path:'events/:id',component:EventDetailsComponent},
+    {path:'events/:id',component:EventDetailsComponent, canActivate:[EventRouteActivator]},
+    {path:'404',component:Error404Component},
     {path:'',redirectTo:'/events',pathMatch:'full'}
 ]
