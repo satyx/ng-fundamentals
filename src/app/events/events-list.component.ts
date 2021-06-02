fa34f12c8459a04b42ae96fa4b0ab45ce31f7ae6
satyx <satyamcrj@gmail.com>
2021-06-02 19:24:51 +0530

    Pre-loading data for components-resolve

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { ToastrService } from "../common/toastr.service";
 import { EventService } from "./shared/event.service";
 
@@ -17,11 +18,12 @@ declare let toastr:any
     `
 })
 export class EventsListComponent implements OnInit{
-    events!: any[];
-    constructor(private eventService: EventService, private toastr: ToastrService){
+    events!: any;
+    constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute){
     }
     ngOnInit(){
-      this.events = this.eventService.getEvents();
+    //   this.eventService.getEvents().subscribe(events => {this.events = events});
+        this.events = this.route.snapshot.data['events']
     }
     handleThumbnailClick(eventName:any)
     {
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,14 @@ import { Routes } from "@angular/router";
 import { Error404Component } from "./app/errors/404.component";
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
 import { EventRouteActivator } from "./app/events/event-details/event-route-activator.service";
+import { EventListResolver } from "./app/events/events-list-resolver.service";
 import { EventsListComponent } from "./app/events/events-list.component";
 import { CreateEventComponent } from "./app/events/shared/create-event.component";
 
 export const appRoutes:Routes = [
-    {path:'events',component:EventsListComponent},
     {path:'events/new',component:CreateEventComponent,
     canDeactivate:['canDeactivateCreateEvent']},
+    {path:'events',component:EventsListComponent, resolve:{events:EventListResolver}},
     {path:'events/:id',component:EventDetailsComponent, canActivate:[EventRouteActivator]},
     {path:'404',component:Error404Component},
     {path:'',redirectTo:'/events',pathMatch:'full'}
